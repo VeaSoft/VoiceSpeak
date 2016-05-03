1129305b90a626b551a5298836dd9aca60ce6a9d
Covenant <covenantchukwudi@gmail.com>
2016-05-03 07:16:38 -0700

    Voice Speak Initial
    Voice Speak Initial Upload

---
+++ VoiceSpeak/Speaker.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using SpeechLib;
+namespace VoiceSpeak
+{
+    class Speaker
+    {
+        SpVoice voice;
+        public void speak(object text)
+        {            
+            lock(this)
+            {
+                voice= new SpVoice();
+                voice.Volume = 100;                
+                voice.Speak((string)text, SpeechVoiceSpeakFlags.SVSFlagsAsync);
+                voice.WaitUntilDone(Timeout.Infinite);
+                
+            }
+
+        }
+        public void pause()
+        {
+            
+            if (voice != null)
+            {
+                voice.Pause();
+            }
+        }
+    }
+}
